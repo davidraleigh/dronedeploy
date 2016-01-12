d6eed7e420684f5267386aa114dc96e50c03e4d7
David Raleigh <davidraleigh@gmail.com>
2016-01-12 13:34:55 -0800

    removing unicode

--- mosaic.py
+++ mosaic.py
@@ -1,10 +1,31 @@
 import cv2
 import numpy as np
 import glob
+import csv
 
-EXAMPLES_DIR = ('./example/')
+EXAMPLE_DIR = './example/'
+IMAGE_DETAILS_FILE = './image_details.txt'
 
-test_files = glob.glob(EXAMPLES_DIR + '*.jpg')
+with open(IMAGE_DETAILS_FILE, 'r') as image_details_csv:
+    details_reader = csv.DictReader(image_details_csv)
+    for row in details_reader:
+        # important columns X est,Y est,Z est,Yaw est,Pitch est,Roll est
+        print row['Filename']
+        print row['X est']
+        print row['Y est']
+        print row['Z est']
+        print row['Yaw est']
+        print row['Pitch est']
+        print row['Roll est']
+        print float(row['X est'])
+        print float(row['Y est'])
+        print float(row['Z est'])
+        print float(row['Yaw est'])
+        print float(row['Pitch est'])
+        print float(row['Roll est'])
+
+
+test_files = glob.glob(EXAMPLE_DIR + '*.jpg')
 
 for image in test_files:
-	print image
+    print image
