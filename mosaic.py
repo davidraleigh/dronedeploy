ccf42561b746cb5516847a903d1d895dfeb025ca
David Raleigh <davidraleigh@gmail.com>
2016-01-12 19:11:40 -0800

    imread and radians

--- mosaic.py
+++ mosaic.py
@@ -2,30 +2,26 @@ import cv2
 import numpy as np
 import glob
 import csv
+import os
+import math
 
-EXAMPLE_DIR = './example/'
+EXAMPLE_DIR = './example'
 IMAGE_DETAILS_FILE = './image_details.txt'
 
 with open(IMAGE_DETAILS_FILE, 'r') as image_details_csv:
     details_reader = csv.DictReader(image_details_csv)
     for row in details_reader:
         # important columns X est,Y est,Z est,Yaw est,Pitch est,Roll est
-        print row['Filename']
-        print row['X est']
-        print row['Y est']
-        print row['Z est']
-        print row['Yaw est']
-        print row['Pitch est']
-        print row['Roll est']
+        img = cv2.imread(os.path.join(EXAMPLE_DIR, row['Filename']))
+        height, width = img.shape[:2]
+        
+        yaw_radians = math.radians(row['Yaw est'])
+        pitch_radians = math.radians(row['Pitch est'])
+        roll_radians = math.radians(row['Roll est'])
+
         print float(row['X est'])
         print float(row['Y est'])
         print float(row['Z est'])
-        print float(row['Yaw est'])
-        print float(row['Pitch est'])
-        print float(row['Roll est'])
 
 
 test_files = glob.glob(EXAMPLE_DIR + '*.jpg')
-
-for image in test_files:
-    print image
