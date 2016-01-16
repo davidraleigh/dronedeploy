e799eac244ab4a5f54bf99a67cacde22efc35efd
David Raleigh <davidraleigh@gmail.com>
2016-01-16 00:44:28 -0800

    cleaning up comments

--- mosaic.py
+++ mosaic.py
@@ -6,7 +6,7 @@ import os
 import math
 from scipy import ndimage
 from scipy import misc
-from osgeo import gdal
+import gdal
 
 #testing 
 import matplotlib.pyplot as plt
@@ -22,6 +22,7 @@ FOCAL_LENGTH_35_MM = 20.0
 # https://support.pix4d.com/hc/en-us/articles/202557469-Step-1-Before-Starting-a-Project-1-Designing-the-Images-Acquisition-Plan-b-Computing-the-Flight-Height-for-a-Given-GSD
 FOCAL_LENGTH_MM = (FOCAL_LENGTH_35_MM * CCD_WIDTH_MM) / 34.6
 
+# http://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
 def order_points(pts):
     # initialzie a list of coordinates that will be ordered
     # such that the first entry in the list is the top-left,
@@ -45,17 +46,14 @@ def order_points(pts):
     # return the ordered coordinates
     return rect
 
+
 def pitch_roll_pts(height, width, RotY, RotX):
     pts = np.matrix([[0, 0, width, width], [height, 0, 0, height],[0, 0, 0, 0]])
-    print pts
-
     centerOffset = np.matrix([[width/2,width/2,width/2,width/2],[height/2,height/2,height/2,height/2],[0, 0, 0, 0]])
     # shift coordinates so that origin is the center of the image
     pts = pts - centerOffset
-    print pts
     # rotate image about the center and add the offset back into the coordinates
     pts = RotY * RotX * pts + centerOffset
-    print pts
     coords = np.array([(pts[0,0], pts[1, 0]), (pts[0,1], pts[1, 1]), (pts[0,2], pts[1, 2]), (pts[0,3], pts[1, 3])])
     return order_points(coords)
 
@@ -89,13 +87,8 @@ def four_point_transform(img, RotY, RotX):
     maxHeight = max(int(heightA), int(heightB))
  
     # compute the perspective transform matrix and then apply it
-    print src_rect
-    print dst_rect
     # M, mask = cv2.findHomography(src_rect, dst_rect, cv2.RANSAC, 5.0) return same result for M
     M = cv2.getPerspectiveTransform(src_rect, dst_rect)
-    print M
-    
-    print M
     warped = cv2.warpPerspective(img, M, (maxWidth, maxHeight))
  
     # return the warped image
@@ -119,42 +112,32 @@ def cmPerPixel(img, pitch_radians, roll_radians, elevation):
     roll_offset = elevation * math.tan(roll_radians)
 
     distance = math.sqrt(math.sqrt(pitch_offset**2 + roll_offset**2) + elevation**2)
-    print distance
     return (CCD_WIDTH_MM * distance * 100)/(FOCAL_LENGTH_MM * width)
 
 #http://stackoverflow.com/questions/25458442/rotate-a-2d-image-around-specified-origin-in-python
 def rotateImage(img, angle, pivot):
     padX = [img.shape[1] - pivot[0], pivot[0]]
-    print padX
     padY = [img.shape[0] - pivot[1], pivot[1]]
-    print padY
     #imgP = np.pad(img, [padY, padX], 'constant')
     imgP = np.pad(img, [padY, padX, [0, 0]], 'constant')
     imgR = ndimage.rotate(imgP, angle, reshape=False)
     return imgR
-    # return imgR[padY[0] : -padY[1], padX[0] : -padX[1]]
-
+    
 with open(IMAGE_DETAILS_FILE, 'r') as image_details_csv:
     details_reader = csv.DictReader(image_details_csv)
-
 
     for row in details_reader:
         # important columns X est,Y est,Z est,Yaw est,Pitch est,Roll est
         jpg_filename = os.path.join(EXAMPLE_DIR, row['Filename'])
-        img = cv2.imread(jpg_filename)
-        height, width = img.shape[:2]
-        print height
-        print width
 
+        # open file for opencv
+        img = cv2.imread(jpg_filename)
+
+        # get yaw, pitch, roll and elevation
         yaw_degrees = float(row['Yaw est']) 
         pitch_radians = math.radians(float(row['Pitch est']))
         roll_radians = math.radians(float(row['Roll est']))
         elevation_meters = float(row['Z est'])
-
-        # yaw rotation (perpendicular to ground)
-        # RotZ = np.matrix([[math.cos(yaw_radians), -math.sin(yaw_radians), 0],
-        #                   [math.sin(yaw_radians), math.cos(yaw_radians) , 0],
-        #                   [0                    , 0                     , 1]])
 
         # pitch rotation (axis through wing perpendicular to flight path)
         RotY = np.matrix([[math.cos(pitch_radians) , 0, math.sin(pitch_radians)],
@@ -166,44 +149,45 @@ with open(IMAGE_DETAILS_FILE, 'r') as image_details_csv:
                           [0, math.cos(roll_radians), -math.sin(roll_radians)],
                           [0, math.sin(roll_radians), math.cos(roll_radians) ]])
 
+        # use rotation matrices to adjust image for pitch and roll errors. Rotate about the center of image.
         warped_image = four_point_transform(img, RotY, RotX)
+
+        # get the pixel directly below the drone (not the center of the image)
         drone_pixel_x, drone_pixel_y = calculateDronePositionPixel(warped_image, pitch_radians, roll_radians)
-        height, width = warped_image.shape[:2]
-        print "warped dimensions"
-        print height
-        print width
+
+        # Ground sampling distance calculations using
         GSD = cmPerPixel(warped_image, pitch_radians, roll_radians, elevation_meters)
 
         #rotate_image = rotateImage(warped_image, yaw_degrees, (drone_pixel_x, drone_pixel_y))
         rotate_image= ndimage.rotate(warped_image, yaw_degrees, (1, 0))
         tif_filename = jpg_filename + '.tif'
+
         cv2.imwrite(tif_filename, rotate_image)
+
+        src_ds = gdal.Open(tif_filename)
+
+        #Open output format driver, see gdal_translate --formats for list
+        format = "GTiff"
+        driver = gdal.GetDriverByName( format )
+
+        #Output to new format
+        dst_ds = driver.CreateCopy( tif_filename, src_ds, 0 )
+
+        print 'Driver: ', dst_ds.GetDriver().ShortName,'/', dst_ds.GetDriver().LongName
+        #Properly close the datasets to flush to disk
+        dst_ds = None
+        src_ds = None
+
+        
         
         height, width = rotate_image.shape[:2]
-        print height
-        print width
 
         # dst = cv2.resize(rotate_image, (width/4, height/4), interpolation = cv2.INTER_CUBIC)
 
         # cv2.imshow('img', dst)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
-        # print pitch_roll_pts(height, width, RotY, RotX)
-        # Rot = RotZ #* RotY * RotX
-
-        # dst = cv2.warpPerspective(dst, Rot, (width, height))
-
-        # Maffine = np.float32([[1, 0, width/2], [0, 1, height/2]])
-        # dst = cv2.warpAffine(dst, Maffine, (width, height))
-
-        # dst = cv2.resize(dst, (width/4, height/4), interpolation = cv2.INTER_CUBIC)
-        # cv2.imshow('img',dst)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-        # print float(row['X est'])
-        # print float(row['Y est'])
-        # print float(row['Z est'])
 
 
-test_files = glob.glob(EXAMPLE_DIR + '*.jpg')
+tif_files = glob.glob(EXAMPLE_DIR + '*.tif')
+print tif_files
